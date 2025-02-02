cae69667ece4558f5533ddc345760b4073852598
Fizza Batool <fizzadcse@gmail.com>
2025-02-02 15:33:37 +0500

    first commit

---
+++ Assets/exit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class QuitWithEsc : MonoBehaviour
+{
+    void Update()
+    {
+        // Check if the Escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    void QuitGame()
+    {
+        // Quit the game
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // Works in Unity Editor
+#else
+        Application.Quit(); // Works in the built application
+#endif
+    }
+}
